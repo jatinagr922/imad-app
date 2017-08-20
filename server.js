5398fdbe6754fa1ab5114335cf0a05288e44dcbc
jatinagr922 <30778967+jatinagr922@users.noreply.github.com>
2017-08-21 00:15:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ content:`
 THIS IS MY FIRST WEB APP
 </p>`}
 };
-function createTemplate (data) {
+function createTemplate (data){
 var title=data.title;    
     var date=data.date;
     var heading=data.heading;
